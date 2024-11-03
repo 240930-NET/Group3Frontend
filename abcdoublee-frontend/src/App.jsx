0028311c0c1033e4a5c6f01102d1482f47ffd3f2
Cuong Vo <dcuongvo@gmail.com>
2024-11-03 15:15:33 -0600

    Add default HomePage

--- abcdoublee-frontend/src/App.jsx
+++ abcdoublee-frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import { React} from 'react';
-import { AuthProvider } from './context/AuthContext';
+//route
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import PropTypes from 'prop-types';
+//component
 import NavBar from './components/NavBar.jsx';  
+//page
 import LoginPage from './pages/LoginPage.jsx'; 
 import RegisterPage from './pages/RegisterPage.jsx'; 
 import UserPage from './pages/UserPage.jsx';
+import HomePage from './pages/HomePage.jsx';
 import './App.css';
 
 
 function App() {
   const ProtectedRoute = ({ element: Element }) => {
     const token = localStorage.getItem('token');
-    return token ? <Element /> : <Navigate to="/login" replace />;
+    return token ? <Element /> : <Navigate to="/" replace />;
   };
 
   ProtectedRoute.propTypes = {
@@ -25,10 +29,7 @@ function App() {
     <Router>
       <NavBar /> 
       <Routes>
-        {/*
-        temporary putting route that has not implement yet here
         <Route path="/" element={<HomePage />} />
-        */}
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/user" element={<ProtectedRoute element={UserPage} />} />
---
+++ abcdoublee-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+//import React from 'react';
+
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to ABCDoubleE Book Tracker</h1>
+      <p>You can track your books in this application.</p>
+    </div>
+  );
+}
+
+export default HomePage;

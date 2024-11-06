aaaa1f502f4d77387c2fe975b27751a2034a9182
Cuong Vo <dcuongvo@gmail.com>
2024-11-06 15:09:03 -0600

    Book Card and Modal working

--- abcdoublee-frontend/src/pages/SearchPage.jsx
+++ abcdoublee-frontend/src/pages/SearchPage.jsx
@@ -2,20 +2,18 @@ import './SearchPage.css';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { apiClient } from '../api/api';
+import BookCard from '../components/BookCard';
 
 function SearchPage() {
     const [books, setBooks] = useState([]);
-    const [databaseBooks, setDatabaseBooks] = useState([]);
     const location = useLocation();
-    const searchTerm = new URLSearchParams(location.search).get('query'); // Get the search term from the URL
+    const searchTerm = new URLSearchParams(location.search).get('query');
 
     useEffect(() => {
         const fetchBooks = async () => {
             try {
                 const response = await apiClient.get(`/google/search`, { params: { query: searchTerm } });
-                // Assuming the API response structure
-                setDatabaseBooks(response.data.booksFromDatabase); // Save database books
-                setBooks(response.data.booksFromGoogle); // Save Google books
+                setBooks([...response.data.booksFromDatabase, ...response.data.booksFromGoogle]);
             } catch (error) {
                 console.error('Error fetching search results:', error);
             }
@@ -29,23 +27,10 @@ function SearchPage() {
     return (
         <div className="search-page">
             <h1>Search Results for "{searchTerm}"</h1>
-            {books.length > 0 || databaseBooks.length > 0 ? (
+            {books.length > 0 ? (
                 <div className="card-list">
                     {books.map((bookInList) => (
-                        <div className="card" key={bookInList.isbn}>
-                            <img src={bookInList.image} alt={bookInList.title} />
-                            <p>Title: {bookInList.title}</p>
-                            <p>Description: {bookInList.description}</p>
-                            <p>ISBN: {bookInList.isbn}</p>
-                        </div>
-                    ))}
-                    {databaseBooks.map((bookInList) => (
-                        <div className="card" key={bookInList.isbn}>
-                            <img src={bookInList.image} alt={bookInList.title} />
-                            <p>Title: {bookInList.title}</p>
-                            <p>Description: {bookInList.description}</p>
-                            <p>ISBN: {bookInList.isbn}</p>
-                        </div>
+                        <BookCard key={bookInList.isbn} book={bookInList} />
                     ))}
                 </div>
             ) : (
